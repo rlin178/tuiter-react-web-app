31d653efc0a2e01d3b23695ffc2622732b314946
Rebecca Lin <Rebecca@Rebeccas-Macbook-Pro.local>
2023-03-04 14:20:06 -0500

    finished 3.1

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -3,10 +3,17 @@ import ExploreComponent from "./explore";
 import NavigationSidebar from "./navigation-sidebar";
 import WhoToFollowList from "./who-to-follow-list";
 import Nav from "../nav";
+import whoReducer
+    from "./reducers/who-reducer";
+import { configureStore }
+    from '@reduxjs/toolkit';
+import {Provider} from "react-redux";
+const store = configureStore(
+    {reducer: {who: whoReducer}});
 
 function Tuiter() {
     return (
-        <div>
+        <Provider store={store}>
             <Nav></Nav>
             <div className="row mt-2">
                 <div className="col-2 col-md-2 col-lg-1 col-xl-2">
@@ -20,7 +27,7 @@ function Tuiter() {
                     <WhoToFollowList/>
                 </div>
             </div>
-        </div>
+        </Provider>
     );
 }
 
