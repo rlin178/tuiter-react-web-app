1c8fefc64b4a0dd8fe0193450525575b50882af2
Rebecca Lin <Rebecca@Rebeccas-Macbook-Pro.local>
2023-03-04 14:10:07 -0500

    ran through lab (must check

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -2,19 +2,23 @@ import React from "react";
 import ExploreComponent from "./explore";
 import NavigationSidebar from "./navigation-sidebar";
 import WhoToFollowList from "./who-to-follow-list";
+import Nav from "../nav";
 
 function Tuiter() {
     return (
-        <div className="row mt-2">
-            <div className="col-2 col-md-2 col-lg-1 col-xl-2">
-                <NavigationSidebar active="explore"/>
-            </div>
-            <div className="col-10 col-md-10 col-lg-7 col-xl-6"
-                 style={{"position": "relative"}}>
-                <ExploreComponent/>
-            </div>
-            <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-                <WhoToFollowList/>
+        <div>
+            <Nav></Nav>
+            <div className="row mt-2">
+                <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+                    <NavigationSidebar active="explore"/>
+                </div>
+                <div className="col-10 col-md-10 col-lg-7 col-xl-6"
+                     style={{"position": "relative"}}>
+                    <ExploreComponent/>
+                </div>
+                <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+                    <WhoToFollowList/>
+                </div>
             </div>
         </div>
     );
