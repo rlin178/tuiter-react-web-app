1ca6acd5b8156eea5fccd23eed3df3cf6bcdd671
Rebecca Lin <Rebecca@Rebeccas-Macbook-Pro.local>
2023-03-05 23:18:33 -0500

    implemented likes

--- src/tuiter/reducers/home-tuits-reducer.js
+++ src/tuiter/reducers/home-tuits-reducer.js
@@ -19,6 +19,8 @@ const homeTuitsSlice = createSlice({
         homeTuitLikeToggle(state, action) {
             const tuit = state.find((tuit) =>
                 tuit._id === action.payload._id)
+
+            tuit.likes = tuit.liked? tuit.likes - 1: tuit.likes + 1;
             tuit.liked = !tuit.liked
         }
     }
